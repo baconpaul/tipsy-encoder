ca5e944a1ba04ae15ccc9b8f6026c69af31ebf82
Paul Walker <paul@pwjw.com>
2023-07-16 09:40:36 -0400

    Add a version header and cmake setup
    Really just run a PR to make sure tests work

---
+++ include/tipsy/tipsy.h
@@ -0,0 +1,14 @@
+/*
+ * tipsy - an encoder for floats to binaries for virtual modular
+ *
+ * This file just includes the sub-files if you want to have the entire
+ * function set available
+ */
+
+#ifndef TIPSY_ENCODER_TIPSY_H
+#define TIPSY_ENCODER_TIPSY_H
+
+#include "version.h"
+#include "binary-to-float.h"
+
+#endif // TIPSY_ENCODER_TIPSY_H
