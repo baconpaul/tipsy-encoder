49e63edf8848132ad73929b58ee4e36da91a2ee7
Paul Walker <paul@pwjw.com>
2023-07-16 12:53:11 -0400

    Draft of the protocol
    A very first draft of a protocol, with so many limitations
    including (1) no state returned by encoders or decoders so
    you can't write a handler, (2) no documentation, (3) etc
    
    but progress

--- include/tipsy/protocol.h
+++ include/tipsy/protocol.h
@@ -11,6 +11,7 @@
 #define TIPSY_ENCODER_PROTOCOL_H
 
 #include <cstdint>
+#include <cstring>
 
 #if __cplusplus >= 201703L
 #define TIPSY_NODISCARD [[nodiscard]]
@@ -23,38 +24,424 @@ namespace tipsy
 
 // We know that our 3-byte-to-binary encoder encodes numbers strictly in the range
 // -10,10 so any valid float outside that range can be used as a sentinel
-static constexpr float MESSAGE_BEGIN{100.f};
+static constexpr float kMessageBeginSentinel{11.f};
+static constexpr float kVersionSentinel{12.f};
+static constexpr float kSizeSentinel{13.f};
+static constexpr float kMimeTypeSentinel{14.f};
+static constexpr float kBodySentinel{15.f};
+static constexpr float kEndMessageSentinel{16.f};
+static constexpr uint16_t kVersion{0x01};
+static constexpr size_t kMaxMimeTypeSize{1024};
 
 struct ProtocolEncoder
 {
-    enum EncoderState
+    enum EncoderResult
     {
         OK
     };
 
     TIPSY_NODISCARD
-    EncoderState initiateMessage(const char *mimeType, const char *messageMetadata,
-                                 uint32_t dataBytes, const unsigned char *const data)
+    EncoderResult initiateMessage(const char *inMimeType, uint32_t inDataBytes,
+                                  const unsigned char *const inData)
     {
+        if (inDataBytes > 1 << 23)
+        {
+            // return MESSAGE_TOO_LARGE
+            return OK;
+        }
+        auto ms = strlen(inMimeType) + 1;
+        if (ms > kMaxMimeTypeSize)
+        {
+            // return MIMETYPE_TOO_LARGE
+            return OK;
+        }
+        if (messageActive)
+        {
+            // return MESSAGE_ALREADY_ACTIVE
+            return OK;
+        }
+
+        messageActive = true;
+        mimeType = inMimeType;
+        mimeTypeSize = ms;
+        data = inData;
+        dataBytes = inDataBytes;
+
+        setState(START_MESSAGE);
+
         return OK;
     }
 
     TIPSY_NODISCARD
-    EncoderState getNextMessageFloat(float &f);
+    EncoderResult getNextMessageFloat(float &f)
+    {
+        switch (encoderState)
+        {
+        case NO_MESSAGE:
+            f = 0;
+            // return INACTIVE
+            break;
+        case START_MESSAGE:
+        {
+            f = kMessageBeginSentinel;
+            pos++;
+            if (pos == 3)
+            {
+                setState(HEADER_VERSION);
+            }
+            // return ACTIVE;
+            break;
+        }
+        case HEADER_VERSION:
+        {
+            if (pos == 0)
+            {
+                f = kVersionSentinel;
+                pos++;
+            }
+            else
+            {
+                unsigned char d[3];
+                d[0] = kVersion & 255;
+                d[1] = (kVersion >> 8) & 255;
+                d[2] = 0;
+                f = threeBytesToFloat(d);
+                setState(HEADER_SIZE);
+            }
+            // return ACTIVE;
+        }
+        break;
+        case HEADER_SIZE:
+        {
+            if (pos == 0)
+            {
+                f = kSizeSentinel;
+                pos++;
+            }
+            else
+            {
+                unsigned char d[3];
+                d[0] = dataBytes & 255;
+                d[1] = (dataBytes >> 8) & 255;
+                d[2] = (dataBytes >> 16) & 255;
+                ;
+                f = threeBytesToFloat(d);
+                setState(HEADER_MIMETYPE);
+            }
+        }
+        break;
+        case HEADER_MIMETYPE:
+        {
+            if (pos == 0)
+            {
+                f = kMimeTypeSentinel;
+                pos++;
+            }
+            else if (pos == 1)
+            {
+                auto sl = mimeTypeSize;
+                unsigned char d[3];
+                d[0] = sl & 255;
+                d[1] = (sl >> 8) & 255;
+                d[2] = (sl >> 16) & 255;
+
+                f = threeBytesToFloat(d);
+                pos++;
+            }
+            else
+            {
+                auto dp = pos - 2;
+                if (dp < mimeTypeSize - 3)
+                {
+                    f = threeBytesToFloat((unsigned char *)(mimeType + dp));
+
+                    pos += 3;
+                    if (pos - 1 == mimeTypeSize)
+                    {
+                        setState(BODY);
+                    }
+                }
+                else
+                {
+                    char d[3]{0, 0, 0};
+                    int i{0};
+                    for (; dp < mimeTypeSize; ++dp)
+                    {
+                        d[i] = mimeType[dp];
+                        i++;
+                    }
+                    f = threeBytesToFloat((unsigned char *)d);
+                    setState(BODY);
+                }
+            }
+        }
+        break;
+        case BODY:
+        {
+            if (pos == 0)
+            {
+                f = kBodySentinel;
+                pos++;
+            }
+            else if (pos - 1 == dataBytes)
+            {
+                setState(END_MESSAGE);
+            }
+            else if (pos - 1 < dataBytes - 3)
+            {
+                f = threeBytesToFloat((unsigned char *)(data + pos - 1));
+
+                pos += 3;
+                if (pos - 1 == dataBytes)
+                {
+                    setState(END_MESSAGE);
+                }
+            }
+            else
+            {
+                auto dp = pos - 1;
+                char d[3]{0, 0, 0};
+                int i{0};
+                for (; dp < dataBytes; ++dp)
+                {
+                    d[i] = data[dp];
+                    i++;
+                }
+                f = threeBytesToFloat((unsigned char *)d);
+                setState(END_MESSAGE);
+            }
+        }
+        break;
+        case END_MESSAGE:
+        {
+            f = kEndMessageSentinel;
+            setState(NO_MESSAGE);
+        }
+        break;
+        }
+
+        return OK;
+    }
 
     TIPSY_NODISCARD
-    EncoderState terminateCurrentMessage();
+    EncoderResult terminateCurrentMessage()
+    {
+        if (encoderState == NO_MESSAGE)
+        {
+            // reutrn NO_MESSAGE_ACTIVE?
+        }
+        setState(NO_MESSAGE);
+        return OK;
+    }
+
+  private:
+    bool messageActive{false};
+    const char *mimeType{nullptr};
+    uint32_t dataBytes{0}, mimeTypeSize{0};
+    const unsigned char *data;
+
+    enum EncoderStates
+    {
+        NO_MESSAGE,
+        START_MESSAGE,
+        HEADER_VERSION,
+        HEADER_SIZE,
+        HEADER_MIMETYPE,
+        BODY,
+        END_MESSAGE
+    } encoderState{NO_MESSAGE};
+
+    int pos{0};
+
+    void setState(EncoderStates s)
+    {
+        encoderState = s;
+        pos = 0;
+    }
 };
 
 struct ProtocolDecoder
 {
-    enum DecoderState
+    enum DecoderResult
     {
-        OK
+        OK,
+        NOT_OK
     };
 
+    bool provideDataBuffer(unsigned char *pdat, size_t psize)
+    {
+        if (decoderState == START_BODY)
+            return false;
+
+        dataStore = pdat;
+        dataStoreSize = psize;
+
+        return true;
+    }
+
+    const char *getMimeType() { return mimetype; }
+
     TIPSY_NODISCARD
-    DecoderState readFloat(float);
+    DecoderResult readFloat(float f)
+    {
+        if (f == kMessageBeginSentinel)
+        {
+            setState(START_HEADER);
+            dataSize = 0;
+            memset(mimetype, 0, sizeof(mimetype));
+            version = -1;
+            return OK;
+        }
+
+        // Use sentinels to force state for next read
+        if (f == kVersionSentinel)
+        {
+            setState(START_VERSION);
+            return OK;
+        }
+        if (f == kSizeSentinel)
+        {
+            setState(START_SIZE);
+            return OK;
+        }
+        if (f == kMimeTypeSentinel)
+        {
+            setState(START_MIMETYPE);
+            return OK;
+        }
+        if (f == kBodySentinel)
+        {
+            setState(START_BODY);
+            return OK;
+        }
+        if (f == kEndMessageSentinel)
+        {
+            setState(DOING_NOTHING);
+            return OK;
+        }
+
+        switch (decoderState)
+        {
+        case DOING_NOTHING:
+            return OK;
+        case START_HEADER:
+            return NOT_OK;
+            break;
+        case START_VERSION:
+            if (pos == 0)
+            {
+                unsigned char d[3];
+                floatToThreeBytes(f, d);
+                version = d[0] + (d[1] << 8);
+                pos++;
+                if (version > 0 && version <= kVersion)
+                {
+                    return OK;
+                }
+                else
+                {
+                    return NOT_OK;
+                }
+            }
+            else
+            {
+                return NOT_OK;
+            }
+            break;
+
+        case START_SIZE:
+            if (pos == 0)
+            {
+                unsigned char d[3];
+                floatToThreeBytes(f, d);
+                dataSize = d[0] + (d[1] << 8) + (d[2] << 16);
+                pos++;
+                return OK;
+            }
+            else
+            {
+                return NOT_OK;
+            }
+            break;
+
+        case START_MIMETYPE:
+        {
+            if (pos == 0)
+            {
+                unsigned char d[3];
+                floatToThreeBytes(f, d);
+                mimetypeSize = d[0] + (d[1] << 8);
+
+                pos++;
+            }
+            else
+            {
+                if (pos > mimetypeSize)
+                {
+                    return NOT_OK;
+                }
+                unsigned char d[3];
+                floatToThreeBytes(f, d);
+
+                if (pos >= kMaxMimeTypeSize - 4)
+                    return NOT_OK;
+
+                auto wp = pos - 1;
+                mimetype[wp] = d[0];
+                mimetype[wp + 1] = d[1];
+                mimetype[wp + 2] = d[2];
+
+                pos += 3;
+                return OK;
+            }
+            break;
+        }
+        case START_BODY:
+            unsigned char d[3];
+            floatToThreeBytes(f, d);
+
+            if (pos < dataStoreSize + 3)
+            {
+                dataStore[pos] = d[0];
+                dataStore[pos + 1] = d[1];
+                dataStore[pos + 2] = d[2];
+                pos += 3;
+                return OK;
+            }
+            else
+            {
+                return NOT_OK;
+            }
+            break;
+        }
+
+        return NOT_OK;
+    }
+
+  private:
+    enum DecoderState
+    {
+        DOING_NOTHING,
+        START_VERSION,
+        START_HEADER,
+        START_SIZE,
+        START_MIMETYPE,
+        START_BODY
+    } decoderState{DOING_NOTHING};
+    int pos{0};
+    int16_t version;
+    uint32_t dataSize;
+    char mimetype[kMaxMimeTypeSize];
+    uint16_t mimetypeSize;
+
+    unsigned char *dataStore{nullptr};
+    int dataStoreSize{0};
+
+    void setState(DecoderState s)
+    {
+        decoderState = s;
+        pos = 0;
+    }
 };
 } // namespace tipsy
 
--- test/protocol.cpp
+++ test/protocol.cpp
@@ -6,18 +6,56 @@
 #include "tipsy/tipsy.h"
 
 #include <cstring>
+#include <iostream>
 
 TEST_CASE("Protocol Encode Simple String")
 {
     const char *mimeType{"application/text"};
-    const char *metaData{"funfun"};
 
     const char *message{"I am the very model of a modern major general"};
 
     tipsy::ProtocolEncoder pe;
 
     // dont forget the null terminatlr
-    auto status =
-        pe.initiateMessage(mimeType, metaData, strlen(message) + 1, (unsigned char *)message);
+    auto status = pe.initiateMessage(mimeType, strlen(message) + 1, (unsigned char *)message);
     assert(status == tipsy::ProtocolEncoder::OK);
+    for (int i = 0; i < 50; ++i)
+    {
+        float nf;
+        auto st = pe.getNextMessageFloat(nf);
+        std::cout << st << " " << nf;
+        if (nf <= 10 && nf >= -10)
+        {
+            unsigned char d[3];
+            tipsy::floatToThreeBytes(nf, d);
+            for (int q = 0; q < 3; ++q)
+                std::cout << " " << (int)d[q];
+        }
+        std::cout << std::endl;
+    }
+}
+
+TEST_CASE("Encode Decode String")
+{
+    const char *mimeType{"application/text"};
+
+    const char *message{"I am the very model of a modern major general"};
+
+    unsigned char buffer[2048];
+
+    tipsy::ProtocolEncoder pe;
+    tipsy::ProtocolDecoder pd;
+    pd.provideDataBuffer(buffer, 2048);
+
+    // dont forget the null terminate
+    auto status = pe.initiateMessage(mimeType, strlen(message) + 1, (unsigned char *)message);
+    assert(status == tipsy::ProtocolEncoder::OK);
+    for (int i = 0; i < 50; ++i)
+    {
+        float nf;
+        auto st = pe.getNextMessageFloat(nf);
+        auto rf = pd.readFloat(nf);
+    }
+    REQUIRE(std::string(pd.getMimeType()) == std::string(mimeType));
+    REQUIRE(std::string((const char *)buffer) == std::string(message));
 }
