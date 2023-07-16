cac6691f5bea63276cc213098b1e141adce88f5f
Paul Walker <paul@pwjw.com>
2023-07-16 10:07:03 -0400

    Add the binary-to-float 3 bytes to float enc/dec
    Low level - still not a protocol. But getting closer!

--- include/tipsy/binary-to-float.h
+++ include/tipsy/binary-to-float.h
@@ -1,6 +1,43 @@
-
+/*
+ * binary-to-float.h
+ *
+ * low level encoding to take 3 unsigned chars and project them to a single valid float
+ * and vice versa.
+ *
+ * The encoding we use is as follows:
+ *
+ * An IEEE float is: lowest 23 bits are fraction, next 8 are exponent, last is sign bit.
+ *
+ */
 
 #ifndef TIPSY_ENCODER_BINARY_TO_FLOAT_H
 #define TIPSY_ENCODER_BINARY_TO_FLOAT_H
+
+#include <cstdint>
+
+namespace tipsy
+{
+inline float threeBytesToFloat(const unsigned char * const d)
+{
+    unsigned char enc[4];
+    enc[0] = d[0];
+    enc[1] = d[1];
+    enc[2] = d[2] & 127;
+    enc[3] = (d[2] & 128) | 63;
+
+    float res = *((float *)&(enc[0]));
+
+    return res;
+}
+
+inline void floatToThreeBytes(float f, unsigned char * const d)
+{
+    auto *y = (unsigned char *)&f;
+
+    d[0] = y[0];
+    d[1] = y[1];
+    d[2] = (y[2] & 127) | (y[3] & 128);
+}
+}
 
 #endif // TIPSY_ENCODER_BINARY_TO_FLOAT_H
---
+++ test/binary.cpp
@@ -0,0 +1,49 @@
+/*
+ * Tests for the simple binary <-> float and back. Although it is a bit expensive
+ * this does test the entire 2^24 bit range.
+ */
+
+#include "catch2.hpp"
+#include "tipsy/binary-to-float.h"
+
+
+TEST_CASE("Binary to Float in range across all binaries")
+{
+    // This test can be a touch slow but works fine
+    unsigned char d[3];
+    float f;
+
+    for (int i = 0; i < 256; i++)
+        for (int j = 0; j < 256; j++)
+            for (int k = 0; k < 256; k++)
+            {
+                d[0] = i;
+                d[1] = j;
+                d[2] = k;
+                f = tipsy::threeBytesToFloat(d);
+                REQUIRE(f > -10);
+                REQUIRE(f < 10);
+                REQUIRE(std::isfinite(f));
+            }
+}
+
+TEST_CASE("Binary to Float Decodable with Fidelity")
+{
+    // This test can be a touch slow but works fine
+    unsigned char d[3], e[3];
+    float f;
+
+    for (int i = 0; i < 256; i++)
+        for (int j = 0; j < 256; j++)
+            for (int k = 0; k < 256; k++)
+            {
+                d[0] = i;
+                d[1] = j;
+                d[2] = k;
+                f = tipsy::threeBytesToFloat(d);
+                tipsy::floatToThreeBytes(f, e);
+                REQUIRE(e[0] == d[0]);
+                REQUIRE(e[1] == d[1]);
+                REQUIRE(e[2] == d[2]);
+            }
+}
