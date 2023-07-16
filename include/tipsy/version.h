0e6aed807b08cdded2215d6d074b0be3e16959e7
Paul <baconpaul@users.noreply.github.com>
2023-07-16 09:45:13 -0400

    Add a version header and cmake setup (#1)
    Really just run a PR to make sure tests work which they now do

---
+++ include/tipsy/version.h
@@ -0,0 +1,15 @@
+/*
+ * Information on version
+ */
+
+#ifndef TIPSY_ENCODER_VERSION_H
+#define TIPSY_ENCODER_VERSION_H
+
+namespace tipsy
+{
+constexpr int TIPSY_VERSION_MAJOR{0};
+constexpr int TIPSY_VERSION_MINOR{1};
+constexpr int TIPSY_VERSION_RELEASE{0};
+}
+
+#endif // TIPSY_ENCODER_VERSION_H
--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,13 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch2.hpp"
 
+#include "tipsy/tipsy.h"
+
 TEST_CASE("Catch is Installed") { REQUIRE(true); }
+
+TEST_CASE("Version is reachable with tipsy.h")
+{
+    REQUIRE(tipsy::TIPSY_VERSION_MAJOR >= 0);
+    REQUIRE(tipsy::TIPSY_VERSION_MINOR >= 0);
+    REQUIRE(tipsy::TIPSY_VERSION_RELEASE >= 0);
+}
